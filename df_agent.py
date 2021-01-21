8b28393a6c430a1093acd93fdb341dbf4d480104
Luigi Di Sotto <luigi.disotto@aitechnologies.it>
2021-01-21 17:37:34 +0100

    fix correctly write output intents and labels

--- df_agent.py
+++ df_agent.py
@@ -6,35 +6,16 @@ import json
 import glob
 import fnmatch
 import io
+from datetime import datetime
 from typing import List, Dict
 
 import google
 import dialogflow_v2
 
+from tqdm import tqdm
+
 logger = logging.getLogger(__name__)
 
-class DialogFlowAgent:
-    def __init__(self, project_name, service_account_filename, **kwargs):
-        if project_name is None or service_account_filename is None:
-            raise ValueError(f'Please provide correct project name and service account file.')
-        self.client = dialogflow_v2.AgentsClient.from_service_account_json(service_account_filename)
-        self.parent = self.client.project_path(project_name)
-        
-    def get_agent(self):
-        zip_raw = None
-        try:
-            operation = self.client.export_agent(self.parent)
-            zip_raw = operation.result().agent_content
-        except google.api_core.exceptions.GoogleAPICallError as e:
-            print("From docs: 'Request failed for any reason'.")
-            raise e
-        except google.api_core.exceptions.RetryError as e:
-            print("From docs: 'Eequest failed due to a retryable error and retry attempts failed'.")
-            raise e
-        except ValueError as e:
-            print("Invalid parameters")
-            raise e
-        return zip_raw
 
 class AgentReader:
     def __init__(self, local_path_or_url, **kwargs):
@@ -123,9 +104,9 @@ class AgentContentJSONReader(AgentContentReader):
     def get_content(self, glob, **kwargs) -> List:
         content = []
         for filename, fp in self.reader.read(glob=glob):
-            print(filename)
             the_json = self.read_json(fp=fp)
         return content
+
 
 class IntentReader:
     def __init__(self, reader, **kwargs):
@@ -134,6 +115,7 @@ class IntentReader:
 
     def get_intents(self, **kwargs):
         pass
+
 
 class JSONIntentReader(IntentReader):
     def __init__(self, reader, **kwargs):
@@ -145,6 +127,31 @@ class JSONIntentReader(IntentReader):
         for js in self.content_reader.get_content(glob='intents/*.json'):
             pass
         return intents
+
+
+class IntentWriter:
+    def __init__(self, **kwargs):
+        super(IntentWriter, self).__init__()
+    
+    def write(self, data, **kwargs):
+        pass
+
+
+class BaseOutputFormatIntentWriter(IntentWriter):
+    def __init__(self, **kwargs):
+        super(BaseOutputFormatIntentWriter, self).__init__(**kwargs)
+
+    def write(self, data: Dict[str, List[str]], **kwargs):
+        base_path = kwargs.pop('output_dir', f'./data-{datetime.now().isoformat()}')
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
+        with open(os.path.join(base_path, 'sent'), 'w') as sent_fp, \
+            open(os.path.join(base_path, 'label'), 'w') as label_fp:
+            for label, texts in data.items():
+                for text in texts:
+                    sent_fp.write(text)
+                    label_fp.write(label)
+
 
 class DialogFlowAgentExport:
     def __init__(self, local_path_or_url, dialogflow=None, **kwargs):
@@ -158,16 +165,52 @@ class DialogFlowAgentExport:
     def get_labels(self, **kwargs):
         pass
 
+class DialogFlowAgentClient:
+    def __init__(self, project_name, service_account, **kwargs):
+        if project_name is None or service_account is None:
+            raise ValueError(f'Please provide correct project name and service account file.')
+        self.client = dialogflow_v2.AgentsClient.from_service_account_json(service_account)
+        self.parent = self.client.project_path(project_name)
+        
+    def get_agent(self):
+        zip_raw = None
+        try:
+            operation = self.client.export_agent(self.parent)
+            zip_raw = operation.result().agent_content
+        except google.api_core.exceptions.GoogleAPICallError as e:
+            print("From docs: 'Request failed for any reason'.")
+            raise e
+        except google.api_core.exceptions.RetryError as e:
+            print("From docs: 'Eequest failed due to a retryable error and retry attempts failed'.")
+            raise e
+        except ValueError as e:
+            print("Invalid parameters")
+            raise e
+        return zip_raw
+
+class DialogFlowAgent:
+    def __init__(self, local_path_or_url, service_account, **kwargs):
+        self.the_agent = DialogFlowAgentClient(project_name=local_path_or_url, service_account=service_account)
+        self.agent_export = DialogFlowAgentExport(local_path_or_url=local_path_or_url, dialogflow=self.the_agent)
+        self.writer = BaseOutputFormatIntentWriter()
+
+    def get_intents(self):
+        return self.agent_export.get_intents()
+
+    def write_intents(self, **kwargs):
+        intents = self.get_intents()
+        self.writer.write(data=intents, **kwargs)
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--local_path_or_url", type=str, required=True, help="The path to local agent zip file (/dir) or gcp project name hosting dialogflow agent.")
     parser.add_argument("--service_account", type=str, required=False, help="The GCP service account path.")
+    parser.add_argument("--output_dir", type=str, required=False, help="The output dir to write data to, eg intent, labels, etc..")
     args = parser.parse_args()
 
-    df = DialogFlowAgent(project_name=args.local_path_or_url, service_account_filename=args.service_account)
-    agent = DialogFlowAgentExport(local_path_or_url=args.local_path_or_url, dialogflow=df)
-    agent.get_intents()
+    agent = DialogFlowAgent(local_path_or_url=args.local_path_or_url, service_account=args.service_account)
+    agent.write_intents(output_dir=args.output_dir)
 
 if __name__ == "__main__":
     main()
--- run_df_agent.sh
+++ run_df_agent.sh
@@ -2,7 +2,9 @@ pip install -r requirements.txt
 
 LOCAL_OR_REMOTE="plg-chatbot"
 SERVICE_ACCOUNT="./dialogflow-fcqosk-plg-chatbot.json"
+OUTPUT_DIR="./outputs"
 
 python df_agent.py \
     --local_path_or_url $LOCAL_OR_REMOTE \
-    --service_account $SERVICE_ACCOUNT
+    --service_account $SERVICE_ACCOUNT \
+    --output_dir $OUTPUT_DIR
