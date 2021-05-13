6364b2d5bd48411c04b5bc124411c75c38c6520e
Diego Giorgini <diego.giorgini@icloud.com>
2021-05-13 15:16:28 +0200

    Update update_intent_test.py to use intent_name instead of id

--- plg/update_intent_test.py
+++ plg/update_intent_test.py
@@ -1,15 +1,14 @@
 from dialogflow_v2.types import Intent
 from dialogflow_v2 import IntentsClient
 import dialogflow_v2
-import os
 from google.oauth2 import service_account
 
-project_id="plg-chatbot"
-intent_id="0db7c106-d5f2-4e45-8fd4-0367bba0d377"
-sa_path="/Users/diego/Progetti/AIT/dialogflow-agent/credentials/dialogflow-fcqosk-plg-chatbot.json"
+project_id = "plg-chatbot"
+intent_name = "your_intent_name"
+sa_path = "../credentials/dialogflow-fcqosk-plg-chatbot.json"
 
 new_training_phrases = []
-sents = open("/Users/diego/Progetti/AIT/dialogflow-agent/sents.txt", "r")
+sents = open("sents.txt", "r")
 for t in sents.readlines():
     t = t.strip()
     parts = [
@@ -22,8 +21,9 @@ for t in sents.readlines():
 
 credentials = service_account.Credentials.from_service_account_file(sa_path)
 client = IntentsClient(credentials=credentials)
-full_intent_name = os.path.join("projects", project_id, "agent/intents", intent_id)
 
-intent = client.get_intent(name=full_intent_name, intent_view=dialogflow_v2.enums.IntentView.INTENT_VIEW_FULL)
-intent.training_phrases.extend(new_training_phrases)
-response = client.update_intent(intent, language_code='en')
+all_intents = client.list_intents(parent=f"projects/{project_id}/agent", intent_view=dialogflow_v2.enums.IntentView.INTENT_VIEW_FULL)
+for intent in all_intents:
+    if intent.display_name == intent_name:
+        intent.training_phrases.extend(new_training_phrases)
+        response = client.update_intent(intent, language_code='en')
